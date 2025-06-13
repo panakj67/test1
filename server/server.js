3d0af056b6a4ffc783da0c265666d095d1e27319
Pankaj Soni <pankajsony9304@gmail.com>
2025-06-13 19:14:08 +0530

    cookie issue solved

--- server/server.js
+++ server/server.js
@@ -1,16 +1,19 @@
 import express from 'express'
 import cors from 'cors'
 import connectDB from './config/db.js';
+import cookieParser from 'cookie-parser';
 import upload from './config/multer.js';
 import dotenv  from 'dotenv'
 dotenv.config();
 
 import userRouter from './routes/userRouter.js'
+import cookieParser from 'cookie-parser';
 const app = express();
 
 connectDB()
 
 app.use(express.json())
+app.use(cookieParser);
 
 app.use(cors({
     origin : process.env.FRONTEND_URL,
